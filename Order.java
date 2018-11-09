61b0ee96349168e61ba1e496eeb50ce32aa400ef
Kevin-Joseph <38056644+Kevin-Joseph@users.noreply.github.com>
2018-11-08 18:07:22 -0600

    Add files via upload

--- Order.java
+++ Order.java
@@ -1,8 +1,56 @@
+import java.util.Vector;
 
 public class Order {
-
+	
+	protected Customer customer;
+	protected String date;
+	protected Vector<Product> products = new Vector<Product>();
+	protected double total;
+	protected boolean hasDiscount;
+	protected double discount;
+	
+	//*****************************************************Constructor
 	public Order() {
-		// TODO Auto-generated constructor stub
+		date ="" + java.time.LocalDate.now();
+		total = 0.0;
+		hasDiscount = false;
+		discount = 0.0;
+	}
+	
+	//****************************************************Getters
+	public double getTotal() {
+		return total;
+	}
+	public boolean checkHasDisount() {
+		return hasDiscount;
+	}
+	public double getDiscount() {
+		return discount;
+	}
+	
+	//*****************************************************Setters
+	public void setCustomer(Customer c) {
+		customer = c;
+	}
+	
+	//add item to products vector
+	public void addProduct(Product p) {
+		products.add(p);
+	}
+	public void removeProduct(int index) {
+		products.remove(index);
+	}
+	
+	//might need a getIndex function.
+	
+	public void setTotal(double d) {
+		total = d;
+	}
+	public void setHasDiscount(boolean b) {
+		hasDiscount = b;
+	}
+	public void setDisount(double d) {
+		discount = d;
 	}
 
 }
